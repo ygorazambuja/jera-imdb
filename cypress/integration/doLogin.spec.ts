7e7a82204bcc2333c6bc1104eee7a66e55930b12
Ygor Azambuja <ygorazambuja@gmail.com>
2022-05-12 11:08:31 -0400

    test: corrige action, separa testes

--- cypress/integration/doLogin.spec.ts
+++ cypress/integration/doLogin.spec.ts
@@ -1,18 +1,23 @@
 describe("Do Login", () => {
-  it("complete login to home", () => {
+  it("should do Login", () => {
     cy.visit("/login");
     cy.get('[data-cy="login"]').should("be.visible");
 
     cy.get('[data-cy="login"]').type("ygoazambuja");
     cy.get('[data-cy="password"]').type("ygoazambuja");
     cy.get('[data-cy="loginButton"]').click();
+  });
 
+  it("should choose profile", () => {
     // cy.url().should("include", "/perfis");
     cy.get(".title > span")
       .should("be.visible")
       .and("contain", "Quem está assistindo ?");
 
     cy.get(":nth-child(1) > .profile-card__avatar > img").click();
+  });
+
+  it("should choose movie", () => {
     // cy.url().should("eq", "http://localhost:5050/");
     cy.get(".navigation-list__title > :nth-child(2)")
       .should("be.visible")
